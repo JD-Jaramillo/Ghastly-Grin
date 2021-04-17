db8aeee6fa69a96a1125b2e480fa89921835f9e5
Shawn Fox <foxeasttrojan06@gmail.com>
2021-04-17 19:17:34 -0400

    cleaning up files deprecated

--- api/app.js
+++ api/app.js
@@ -12,9 +12,9 @@ const options = { cors: { origin: "*" } };
 const io = require("socket.io")(httpServer, options);
 
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var testAPIRouter = require("./routes/testAPI");
+const routes = require("./controllers");
+const sequelize = require("./config/connection");
+
 var app = express();
 
 const PORT = process.env.PORT || 3001;
@@ -27,10 +27,6 @@ const sess = {
 
 app.use(session(sess));
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
 app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
@@ -38,9 +34,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use("/testAPI", testAPIRouter);
+app.use(routes);
+app.use(compression());
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
@@ -64,6 +59,8 @@ io.on("connection", socket => {
   })
 });
 
-httpServer.listen(3001);
+sequelize.sync({ force: false }).then(() => {
+  httpServer.listen(3001);
+})
 
 module.exports = app;
