7573fedb64763f438043f3468125a211efa9e18c
Eric Hurley <erichurley421@gmail.com>
2021-04-17 13:35:46 -0400

    base header and navlink setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
-import logo from './logo.svg';
-import './App.css';
 import io from "socket.io-client"
+import Header from "../src/components/Header/index";
+import './App.css';
 const socket = io.connect("127.0.0.1:3001/");
 
 function App() {
@@ -18,21 +18,11 @@ function App() {
   
   socket.emit("welcome", "connected");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div>
+        <Header />
         <p>
           {apiFetch}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
---
+++ client/src/components/Header/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Nav from "../Nav";
+
+function Header() {
+  return (
+    <div>
+      <h1>Ghastly Grin</h1>
+      <Nav />
+    </div>
+
+  )
+}
+
+export default Header;
