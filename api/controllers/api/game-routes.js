20e419ae42051de6edc0f3188644c263727b54bc
Shawn Fox <foxeasttrojan06@gmail.com>
2021-04-18 00:46:58 -0400

    added routes

--- api/controllers/api/game-routes.js
+++ api/controllers/api/game-routes.js
@@ -1,7 +1,58 @@
+const withAuth = require('../../utils/auth');
+const { Game, User, Player } = require("../../models");
+
+
 const router = require('express').Router();
 
 router.get("/", function(req, res, next) {
   res.send("API is working properly");
 });
+
+router.post("/", withAuth, async (req, res) => {
+  try {
+    const gameInit = await Game.create({
+      game_owner: req.session.id
+    });
+    const gameFind = await Game.findOne({
+      where: {
+        game_owner: req.session.id
+      }
+    })
+    const playerInit = await Player.create({
+      score: 0,
+      game_id: gameFind.id,
+      user_id: req.session.id
+    })
+    const playerFind = await Player.findOne({
+      where: {
+        user_id: req.session.id
+      }
+    })
+    req.session.save(() => {
+      req.session.player_id = playerFind.id;
+    })
+    //Probably wont work, maybe make an array?
+    res.status(200).json(gameInit, playerInit)
+
+  } catch (err) {
+    res.status(400).json(err);
+  }
+})
+
+router.delete("/", withAuth, async (req, res) => {
+  try {
+    const gameDelete = await Game.destroy({
+      where: {
+        game_owner: req.session.id,
+      }
+    });
+    if (!gameDelete) {
+      res.status(404).json({ message: "no game found with this id!"});
+    }
+    res.status(200).json(gameDelete)
+  } catch (err) {
+    res.status(400).json(err);
+  }
+})
 
 module.exports = router;
--- api/controllers/api/player-routes.js
+++ api/controllers/api/player-routes.js
@@ -1,7 +1,71 @@
+const { Sequelize } = require('sequelize/types');
+const { Player } = require('../../models');
+const withAuth = require('../../utils/auth');
+
 const router = require('express').Router();
 
 router.get("/", function(req, res, next) {
   res.send("API is working properly");
 });
+
+// MUST BE CALLED WITH A GAME_ID IN BODY
+router.post("/", withAuth, async (req, res) => {
+  try {
+    const playerInit = await Player.create({
+      score: 0,
+      game_id: req.body.game_id,
+      user_id: req.session.id
+    });
+    const playerFind = await Player.findOne({
+      where: {
+        user_id: req.session.id
+      }
+    });
+    req.session.save(() => {
+      req.session.player_id = playerFind.id;
+    });
+    res.status(200).json(playerInit)
+  } catch (err) {
+    res.status(500).json(err);
+  }
+})
+
+// MUST HAVE USER ID IN PARAMS, AND BODY SCORE
+router.put("/score/:id", withAuth, async (req, res) => {
+  try {
+    const updatePlayer = await Player.update(
+      { score: Sequelize.literal(`score + ${req.body.score}`)},
+      {
+        where: {
+          user_id: req.params.id
+        }
+      }
+    )
+    if (!updatePlayer) {
+      res.status(404).json({ message: "No player found with this id!"});
+      return;
+    }
+    res.status(200).json(updatePlayer);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+})
+
+router.delete("/:id", withAuth, async (req, res) => {
+  try {
+    const deletePlayer = await Player.destroy(
+      { where: {
+        user_id: req.params.id
+      }}
+    );
+    if (!deletePlayer) {
+      res.status(404).json({ message: "No player found with this id!"});
+      return;
+    };
+    res.status(200).json(deletePlayer);
+  } catch (err) {
+    res.status(500).json(err)
+  }
+})
 
 module.exports = router;
