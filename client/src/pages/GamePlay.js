bdf36a9836620818c4282cb850c5e029c10de2c9
Eric Hurley <erichurley421@gmail.com>
2021-04-20 13:51:10 -0400

    starting logic for getting random questions and answers for cards

---
+++ client/src/pages/GamePlay.js
@@ -0,0 +1,76 @@
+import React from "react";
+import BlackCard from "../components/BlackCard";
+import WhiteCard from "../components/WhiteCard";
+
+function GamePlay() {
+  const cardFiller = [{
+      id: 1,
+      value: "one card",
+    }, {
+      id: 2,
+      value: "two card",
+    }, {
+      id: 3,
+      value: "three card",
+    }, {
+      id: 4,
+      value: "four card",
+    }, {
+      id: 5,
+      value: "five card",
+    }, {
+      id: 6,
+      value: "six card",
+    }, {
+      id: 7,
+      value: "seven card",
+    }, {
+      id: 8,
+      value: "eight card",
+    }, {
+      id: 9,
+      value: "nine card",
+    }, {
+      id: 10,
+      value: "ten card",
+    }, {
+      id: 11,
+      value: "eleven card",
+    }, {
+      id: 12,
+      value: "twelve card"
+  }];
+
+  const questions = [
+    "what",
+    "who",
+    "when",
+    "where",
+    "how"
+  ]
+
+  const randomCards = () => {
+    for (let i = 0; i < 8; i++) {
+      let hand = cardFiller.value[i];
+      return Math.floor(Math.random(hand) * cardFiller.length)
+    }
+  }
+
+  const singleCard = () => {
+    let question = [Math.floor(Math.random(questions) * questions.length)]
+    return question; 
+  }
+
+  // 7 White Cards per player
+  // 1 Black card per round
+  // Players hand function, call randomCards for each player
+  return (
+    <>
+    {/* Pass card attributes through */}
+      <BlackCard onClick={singleCard} />
+      <WhiteCard onClick={randomCards} />
+    </>
+  )
+}
+
+export default GamePlay;
