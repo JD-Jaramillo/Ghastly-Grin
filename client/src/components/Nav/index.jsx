34e1b97893d726c1c86042bc4e772f73f527ec7a
Shawn Fox <foxeasttrojan06@gmail.com>
2021-04-21 16:11:38 -0400

    login auth set in nav bar

--- client/src/components/Nav/index.jsx
+++ client/src/components/Nav/index.jsx
@@ -6,6 +6,16 @@ import "./style.css";
 
 function Nav() {
   const [loggedIn, setLoggedIn] = useState();
+
+  const logOut = () => {
+    axios.post('/api/user/logout', { withCredentials: true })
+    .then(() => {
+      console.log("Logged Out")
+      document.location.replace('/')
+    })
+    .catch(err => console.log(err))
+  }
+
   useEffect(() => {
     axios.get('/api/user')
       .then(res => {
@@ -20,7 +30,7 @@ function Nav() {
             <Link className="nav-link" to="/CreateGame">Create/Join Game</Link>
           </li>
           <li className="nav-item">
-            
+          <div onClick={logOut} className="nav-link">Log-Out</div>
           </li>
         </>
         :
