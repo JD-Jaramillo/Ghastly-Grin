855933fc51b113f11c940b2d368fec5ff5464d36
John Jaramillo <jaramillo784@gmail.com>
2021-04-22 17:37:30 -0400

    fixed the footer

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -5,7 +5,7 @@ import './Footer.css';
 function Footer() {
     return (
         <footer id="footer" className='container-fluid'>
-            <h4><span><a target="_blank" href="https://github.com/JD-Jaramillo/Ghastly-Grin">Ghastly Grin</a></span></h4>
+            <a target="_blank" href="https://github.com/JD-Jaramillo/Ghastly-Grin">Ghastly Grin</a>
         </footer>
     );
 }
