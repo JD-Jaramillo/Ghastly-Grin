eed8a2daf59ad8bbeab7086a9641797e1611c8d2
John Jaramillo <jaramillo784@gmail.com>
2021-04-22 20:02:07 -0400

    fixed the hmaburgerBars and removed body-all

--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -4,25 +4,15 @@ import "./style.css"
 import { Link } from 'react-router-dom';
 
 function Header() {
-  const [hamburgerState, setHamburgerState] = useState(true);
+  
 
   return (
-    <div className="banner .container-fluid">
+    <nav className="desktop-navigation .container-fluid">
       <img className="headLogo" src="/GhastlyGrinLogoTrans_green.png" alt="logo" />
       <Link to="/">
         <h1 id="title" className="row">Ghastly Grin</h1>
       </Link>
-      <nav className="nav-group">
-        <div onClick={() => setHamburgerState(!hamburgerState)} className={hamburgerState ? null : 'open'} id="nav-icon2">
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-        </div>
-        {hamburgerState ? null : <div>New-Home</div>}
-        {hamburgerState ? null : <div>New-Lobby</div>}
+      <div className="nav-group">
         <Link to="/">
           <span><a className="nav-links">Home</a></span>
         </Link>
@@ -32,11 +22,9 @@ function Header() {
         <Link to="/Lobby">
           <span><a className="nav-links">Exit Game</a></span>
         </Link >
-      </nav>
+      </div>
       <Nav />
-    </div>
-
-
+    </nav>
   )
 }
 
