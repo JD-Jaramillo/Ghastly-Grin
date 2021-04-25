9b7ff053d3d63994b98a80f51b0d3d7c3eee8817
Shawn Fox <foxeasttrojan06@gmail.com>
2021-04-24 20:34:34 -0400

    session signup

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -36,7 +36,7 @@ router.post("/", async (req, res) => {
     req.session.user_id = dbUserId.id
     req.session.loggedIn = true;
     // res.send(req.session)
-    res.status(200).json(dbUserId);
+    res.status(200).json(req.session);
   } catch (err) {
     console.log(err);
     res.status(500).json(err);
