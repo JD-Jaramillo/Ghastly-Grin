2e5fbc1a0c4cba151b1b92dbccd12c8303b19430
Shawn Fox <foxeasttrojan06@gmail.com>
2021-04-27 23:39:38 -0400

    get *

--- app.js
+++ app.js
@@ -43,11 +43,13 @@ app.use(cookieParser('secret'));
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
-  app.get('*', (req, res) => {
-    res.sendFile('client/build');
-  });
 }
 
+app.use(routes);
+app.use(compression());
+app.get('*', (req, res) => {
+  res.sendFile('client/build');
+});
 // if (process.env.NODE_ENV === 'production') {
 //   app.use(express.static(path.join(__dirname, 'client/build')));
 //   app.get('*', function(req, res) {
@@ -55,23 +57,6 @@ if (process.env.NODE_ENV === "production") {
 //   });
 // }
 
-app.use(routes);
-app.use(compression());
-// catch 404 and forward to error handler
-// app.use(function (req, res, next) {
-//   next(createError(404));
-// });
-
-// error handler
-// app.use(function (err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.json({ error: err});
-// });
 
 // io.on("connection", socket => {
 //   console.log(socket.id);
